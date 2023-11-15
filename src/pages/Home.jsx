5e9eb934a9ab5d989b3e84e3e2f9363af5969126
caiomelotec <caio.melotec@icloud.com>
2023-11-15 16:07:21 +0100

    Home.css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import axios from "axios";
 import { useState, useEffect } from "react";
-import "../styles/Home.css";
+import "../styles/home.css";
 import { Link } from "react-router-dom";
 import { useSearch } from "../store/searchStore";
 
